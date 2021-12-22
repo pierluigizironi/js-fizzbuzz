1a1c001e43eed6b77217255d56e31108d5bc8be7
pierluigizironi <zironipierluigi@gmail.com>
2021-12-22 15:29:18 +0100

    human code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 console.log('JS OK')
+
+/* Scrivi un programma che stampi i numeri da 1 a 100,
+ma per i multipli di 3 stampi Fizz al posto del numero e per i multipli di 5 stampi Buzz.
+Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.*/
+
+// 1. Creo un ciclo che permetta la creazione di numeri che vadano da 1 a 100
+// 2. Metto una prima condizione che faccia apparire la scritta Fizz al posto dei multipli di 3
+// 3. Metto una seconda condizione che faccia apparire la scritta Buzz per i multipli di 5
+// 4. Se soddisfano i punti 2 e 3, diventeranno Fizzbuzz
+
